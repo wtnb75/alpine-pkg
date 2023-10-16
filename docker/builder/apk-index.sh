979b839af238ccc770f600f7a64a15c732051e18
Watanabe Takashi <wtnb75@gmail.com>
2023-10-17 08:41:21 +0900

    fix failed case (#352)
    close #349 
    
    * fix failed case
    
    * update
    
    * bash
    
    * test
    
    * output
    
    * artifact
    
    * output
    
    * chmod

--- docker/builder/apk-index.sh
+++ docker/builder/apk-index.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
+set -eu
+set -o pipefail
 . /etc/abuild.conf
 . $HOME/.abuild/abuild.conf
 
-for idx in $(find ${REPODEST}/apk/$(uname -m) -type f -name APKINDEX.tar.gz); do
+for idx in $(find ${REPODEST-/home/packager/packages}/apk/$(uname -m) -type f -name APKINDEX.tar.gz); do
   [ -f "${idx}" ] || continue
   d=$(dirname $idx)
   apk index -o ${idx} ${d}/*.apk
